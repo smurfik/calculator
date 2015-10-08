cda5316ac90ae843fb52add3fafdb6a12fcd3935
smurfik <smurfik@mlpinit.com>
2015-10-08 14:19:33 -0700

    add while loop

--- calculator.rb
+++ calculator.rb
@@ -1,25 +1,32 @@
-puts "Hello, I'm a calculator! What would you like to do?"
+response = ""
 
-response = gets.chomp
+while response != "done" || response != "exit"
+  puts "Hello, I'm a calculator! What would you like to do?"
 
-if response == "add" || response == "+"
-  puts "Give me a number"
-  num1 = gets.chomp.to_i
-  puts "Give me another number"
-  num2 = gets.chomp.to_i
-  puts "Let me see... #{num1} + #{num2} is #{num1 + num2}."
-elsif response == "subtract" || response == "-"
-  puts "Give me a number"
-  num1 = gets.chomp.to_i
-  puts "Give me another number"
-  num2 = gets.chomp.to_i
-  puts "Let me see... #{num1} - #{num2} is #{num1 - num2}."
-elsif response == "multiply" || response == "*"
-  puts "Give me a number"
-  num1 = gets.chomp.to_i
-  puts "Give me another number"
-  num2 = gets.chomp.to_i
-  puts "Let me see... #{num1} * #{num2} is #{num1 * num2}."
-else
-  puts "I'm not sure what I'll do"
+  response = gets.chomp
+
+  if response == "add" || response == "+"
+    puts "Give me a number"
+    num1 = gets.chomp.to_i
+    puts "Give me another number"
+    num2 = gets.chomp.to_i
+    puts "Let me see... #{num1} + #{num2} is #{num1 + num2}."
+  elsif response == "subtract" || response == "-"
+    puts "Give me a number"
+    num1 = gets.chomp.to_i
+    puts "Give me another number"
+    num2 = gets.chomp.to_i
+    puts "Let me see... #{num1} - #{num2} is #{num1 - num2}."
+  elsif response == "multiply" || response == "*"
+    puts "Give me a number"
+    num1 = gets.chomp.to_i
+    puts "Give me another number"
+    num2 = gets.chomp.to_i
+    puts "Let me see... #{num1} * #{num2} is #{num1 * num2}."
+  elsif response == "done" || response == "exit"
+    exit
+  else
+    puts "I'm not sure what I'll do, could you be more specific?"
+  end
+
 end
