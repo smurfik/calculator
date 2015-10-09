02d50ac410b5497d6316c00c85d1ee0faa77e9ed
smurfik <smurfik@mlpinit.com>
2015-10-08 17:00:37 -0700

    cleaned up the comments

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,5 @@
 while true 
-  puts "Hello, I'm a calculator! Please add numbers in one line with spaces."
+  puts "Hello, I'm a calculator! Please add or subtract numbers in one line with spaces."
 
   response = gets.chomp.split
 
@@ -17,25 +17,12 @@ while true
          answer = string.to_i - answer 
        end
      end
-#   elsif response == "multiply" || response == "*"
-#     puts "Give me a number"
-#     num1 = gets.chomp.to_i
-#     puts "Give me another number"
-#     num2 = gets.chomp.to_i
-#     puts "Let me see... #{num1} * #{num2} = #{num1 * num2}."
-#   elsif response == "divide" || response == "/"
-#     puts "Give me a number"
-#     num1 = gets.chomp.to_i
-#     puts "Give me another number"
-#     num2 = gets.chomp.to_i
-#     puts "Let me see... #{num1} / #{num2} = #{num1 / num2}."
+    elsif response[0] == "done" || response[0] == "exit"
+      exit
+    else
+      puts "I'm not sure what to do, could you be more specific?"
+    end
 
-  elsif response[0] == "done" || response[0] == "exit"
-     exit
-  else
-    puts "I'm not sure what to do, could you be more specific?"
-  end
-  
-   puts "Let me see... the result is #{answer}"
+  puts "Let me see... the result is #{answer}"
 
 end
